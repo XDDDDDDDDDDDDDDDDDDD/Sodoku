1771f8129e7579f41d3591b60202164a5822dc70
ChrisUwe <C.Kracht1@web.de>
2017-05-11 10:33:02 +0200

    Dropdown

--- Start.php
+++ Start.php
@@ -49,6 +49,33 @@
     background-color: #111;
     }
 
+    li.dropdown {
+      display: inline-block;
+  	}
+
+    .dropdown-content {
+      display: none;
+      position: absolute;
+      /*background-color: #f9f9f9;*/
+      min-width: 160px;
+      box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+      z-index: 1;
+    }
+
+    .dropdown-content a {
+      color: white;
+      padding: 12px 16px;
+      text-decoration: none;
+      display: block;
+      text-align: left;
+    }
+
+    .dropdown-content a:hover {background-color: black}
+
+    .dropdown:hover .dropdown-content {
+      display: block;
+    }
+
     div.difficulty {
     margin: 5px;
     border: 1px solid #ccc;
@@ -70,18 +97,11 @@
     text-align: center;
     }
 
-    article {
-
-    }
 
 	.disabled {
     pointer-events:none; //This makes it not clickable
     opacity:0.6;         //This grays it out to look disabled
 	}
-
-
-
-
 
   </style>
 <body>
@@ -110,9 +130,6 @@
 
 	}
 
-
-
-
 ?>
 
 
@@ -120,7 +137,13 @@
   <ul>
     <li><a href="Start.php" class="active" href="#Start">Start</a></li>
     <li><a href="Profil.php"class=<?PHP echo $act; ?> href="#Profil">Profil</a></li>
-    <li><a href="Bestenliste"class="active" href="#Bestenliste">Bestenliste</a></li>
+    <li class="dropdown">
+      <a href="Bestenliste"class="active" >Bestenliste</a>
+      <div class="dropdown-content">
+        <a href="besttime.php"> Bestenliste Zeiten </a>
+        <a href="bestgames.php"> Bestenliste Spiele </a>
+        <a href="bestrank.php"> Bestenliste ELO </a>
+    </li>
     <li><a href="Tutorial.php" class="active" href="#Regeln">Regeln</a></li>
     <li><a href="Impressum.php" class="active" href="#Impressum">Impressum</a></li>
   </ul>
@@ -128,7 +151,7 @@
   <article style="float:left; margin-left:30%; margin-top:60px">
     <div class="difficulty">
       <a target="_blank" href="Leicht">
-        <img src="pictures/star.jpg" alt="Trolltunga Norway" width="400" height="400">
+        <img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRsOUswpN22OQbjuhfnP-SZb4-B4zam9kVu1Al1T5QDie4ItXTH" alt="Trolltunga Norway" width="400" height="400">
       </a>
       <div class="desc" style="color:white">Leicht</div>
     </div>
@@ -137,7 +160,7 @@
   <article style="float:right; margin-right:30%; margin-top:60px">
     <div class="difficulty">
       <a target="_blank" href="Mittel">
-        <img src="pictures/star.jpg" alt="Forest" width="400" height="400">
+        <img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRsOUswpN22OQbjuhfnP-SZb4-B4zam9kVu1Al1T5QDie4ItXTH" alt="Forest" width="400" height="400">
       </a>
       <div class="desc" style="color:white">Mittel</div>
     </div>
@@ -146,7 +169,7 @@
   <article style="float:left; display:block; margin-left:30%; margin-top:20px">
     <div class="difficulty">
       <a target="_blank" href="Schwer">
-        <img src="pictures/star.jpg" alt="Northern Lights" width="400" height="400">
+        <img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRsOUswpN22OQbjuhfnP-SZb4-B4zam9kVu1Al1T5QDie4ItXTH" alt="Northern Lights" width="400" height="400">
       </a>
       <div class="desc" style="color:white">Schwer</div>
     </div>
@@ -155,7 +178,7 @@
   <article style="float:right; display:block; margin-right:30%; margin-top:20px">
     <div class="difficulty">
       <a target="_blank" href="Extrem">
-        <img src="pictures/star.jpg" alt="Mountains" width="400" height="400">
+        <img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRsOUswpN22OQbjuhfnP-SZb4-B4zam9kVu1Al1T5QDie4ItXTH" alt="Mountains" width="400" height="400">
       </a>
       <div class="desc" style="color:white">Extrem</div>
     </div>
