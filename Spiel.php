72c9e80059ad99990a09c9e28b7136ff06db0529
ChrisUwe <C.Kracht1@web.de>
2017-05-11 11:13:37 +0200

    export navigation

--- Spiel.php
+++ Spiel.php
@@ -26,52 +26,6 @@
       display: block;
     }
 
-    ul {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-    }
-
-    li {
-    float: left;
-    }
-
-    li a {
-    display: block;
-    color: white;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-    }
-
-    li a:hover {
-    background-color: #111;
-    }
-    
-    .dropdown-content {
-      display: none;
-      position: absolute;
-      /*background-color: #f9f9f9;*/
-      min-width: 160px;
-      box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-      z-index: 1;
-    }
-
-    .dropdown-content a {
-      color: white;
-      padding: 12px 16px;
-      text-decoration: none;
-      display: block;
-      text-align: left;
-    }
-
-    .dropdown-content a:hover {background-color: black}
-
-    .dropdown:hover .dropdown-content {
-      display: block;
-    }
-
     table {
       margin:1em auto;
       color:white;
@@ -120,21 +74,10 @@
     </div>
   </header>
 
-  <ul>
-    <li><a href="Start.php" class="active" href="#Start">Start</a></li>
-    <li><a href="Profil.php"class="active" href="#Profil">Profil</a></li>
-    <li class="dropdown">
-      <a href="Bestenliste"class="active" >Bestenliste</a>
-      <div class="dropdown-content">
-        <a href="besttime.php"> Bestenliste Zeiten </a>
-        <a href="bestgames.php"> Bestenliste Spiele </a>
-        <a href="bestrank.php"> Bestenliste ELO </a>
-      </div>
-    </li>
-    <li><a href="Tutorial.php" class="active" href="#Tutorial">Regeln</a></li>
-    <li><a href="Impressum.php" class="active" href="#Impressum">Impressum</a></li>
-  </ul>
-
+<!-- Include der Navigationbar und des dazu gehörigen Styles -->
+  <?php
+    include("include/navigationbar.html");
+  ?>
 
   <table>
     <tr>
