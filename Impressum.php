13cb5f023a174eba0c51d62add6ee0d5db0b17a5
ChrisUwe <C.Kracht1@web.de>
2017-05-12 23:34:22 +0200

    edit dok + export navigation

--- Impressum.php
+++ Impressum.php
@@ -26,59 +26,17 @@
       display: block;
     }
 
-    ul {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-    }
 
-    li {
-    float: left;
-    }
-
-    li a {
-    display: block;
-    color: white;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-    }
-
-    li a:hover {
-    background-color: #111;
-    }
-	  
-    .dropdown-content {
-      display: none;
-      position: absolute;
-      min-width: 160px;
-      box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-      z-index: 1;
-    }
-
-    .dropdown-content a {
-      color: white;
-      padding: 12px 16px;
-      text-decoration: none;
-      display: block;
-      text-align: left;
-    }
-
-    .dropdown-content a:hover {background-color: black}
-
-    .dropdown:hover .dropdown-content {
-      display: block;
-    }
-
-	.disabled {
-    pointer-events:none; //This makes it not clickable
-    opacity:0.6;         //This grays it out to look disabled
+    .disabled {
+      pointer-events:none; //This makes it not clickable
+      opacity:0.6;         //This grays it out to look disabled
 	}
 
   </style>
 <body>
 
+<!-- Die Abfrage überprüft ob ein Nutzer angemeldet ist. Ist dies der Fall so kann er auf der Navigationbar sein Profil aufrufen, andernfalls
+       wird der Reiter "Profil" disabled. -->
 <?PHP
 
 	session_start();
@@ -102,25 +60,12 @@
 
 ?>
 
+<!-- Include der Navigationbar und des dazu gehörigen Styles -->
+  <?php
+    include("include/navigationbar.html");
+  ?>
 
-
-  <ul>
-    <li><a href="Start.php" class="active" href="#Start">Start</a></li>
-    <li><a href="Profil.php"class=<?PHP echo $act; ?> href="#Profil">Profil</a></li>
-    <li class="dropdown">
-      <a href="Bestenliste"class="active" >Bestenliste</a>
-      <div class="dropdown-content">
-        <a href="besttime.php"> Bestenliste Zeiten </a>
-        <a href="bestgames.php"> Bestenliste Spiele </a>
-        <a href="bestrank.php"> Bestenliste ELO </a>
-      </div>
-    </li>
-    <li><a href="Tutorial.php" class="active" href="#Regeln">Regeln</a></li>
-    <li><a href="Impressum.php" class="active" href="#Impressum">Impressum</a></li>
-  </ul>
-
-
-
+<!-- Ausgabe der Betriebsinformationen -->
   <article style="color:white">
     <h1 style="text-decoration: underline"> Impressum </h1>
     <p style="font-size:130%"> SudokuOnline ist ein Produkt der FürDummies GmbH. </p>
@@ -132,11 +77,9 @@
       Jan Getschmann <br/>
       Christian Kracht <br/>
       Carolin Kuessner </p>
-
   </article>
 
-
-
+<!-- Ausgabe der Fußzeile, in der Unternehmensinformationen enthalten sind. -->
   <footer style="position: absolute; bottom: 5px">
     Copyright &copy; Getschmann, Kracht, Kuessner </br>
     31789 Hameln Sudoku für Dummies GmbH </br>
